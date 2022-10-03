f4450867c87386861391372865fa4de7b1005b40
Suresh Choudhary <mesuresh318@gmail.com>
2022-10-03 22:13:29 +0530

    per

--- practice1.py
+++ practice1.py
@@ -3,3 +3,14 @@ b = 2
 c = "suresh"
 print(a + b)
 print(c)
+
+
+per = int(input("enter the value:-"))
+if per >= 60:
+    print = ("first div")
+elif per >= 45:
+    print = ("second div")
+elif per >= 35:
+    print = ("third div")
+else:
+    print("fail")
